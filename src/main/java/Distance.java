a689991b38982a80fdc15e510cb0a9c02e741437
vikrant220 <vikrant19398@iiitd.ac.in>
2023-12-12 13:00:07 +0530

    Second Commit - Akash & Vikrant

--- src/main/java/Distance.java
+++ src/main/java/Distance.java
@@ -1,15 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 enum Unit{
     KM,METER,CM
         }
 
+
+
 public class Distance {
     final private int value;
+    final static public Map<Unit, Integer> map = new HashMap<>();
+
+    static {
+        map.put(Unit.METER,100);
+        map.put(Unit.KM,100000);
+        map.put(Unit.CM,1);
+
+    }
 
 
     public Distance(int value){
-        this.value = value;
+        this.value = value*100;
     }
 
 //    public  int HashCode(){
@@ -30,12 +42,15 @@ public class Distance {
     }
 
     public Distance(int value, Unit unit) {
-        if(unit == Unit.KM)
-            this.value = value*1000;
-        else if (unit==Unit.CM) {
-            this.value = value/100;
-        } else
-            this.value = value;
+
+        this.value = map.get(unit)*value;
+
+//        if(unit == Unit.KM)
+//            this.value = value*1000;
+//        else if (unit==Unit.CM) {
+//            this.value = value/100;
+//        } else
+//            this.value = value;
 
     }
 
